f738561a1cecb6e2bd03da6f1325779170d95d5e
Stanislav Fedotov <me@moneyseeker.ru>
2016-08-17 16:59:04 +0300

    Fixing typos
    Change-Id: I103bd3a32f4dec1d60955dbdb3e6ea9fff3b6ea9

--- hennessy-vendor-blobs.mk
+++ hennessy-vendor-blobs.mk
@@ -1,15 +1,15 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/app,system/app)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/proprietary/app,system/app)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/lib64,system/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/proprietary/lib64,system/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/proprietary/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/proprietary/xbin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hermes/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/hennessy/proprietary/vendor,system/vendor)
